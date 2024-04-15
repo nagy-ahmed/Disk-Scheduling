6fa14f92b9ffd0471e008a302a36b79b83494dd0
Ahmed Nagy <dawood101dawood@gmail.com>
2024-04-15 22:54:58 +0200

    show multi figure

--- PLOT.py
+++ PLOT.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
 
-def show (ordered_request,algorithm):
-    order=[]
-    for i in range(len(ordered_request)):
-        order.append(i+1)
+def show(ordered_request, algorithm):
+    order = list(range(1, len(ordered_request) + 1))
 
-    # print("plot ", algorithm, ordered_request)
-    plt.cla()
+    # Create a new figure for each plot
+    plt.figure()
+
     # Create the line plot
     plt.plot(ordered_request, order, marker='o', color='b')
 
     # Set plot labels and title
     plt.xlabel("Request")
     plt.ylabel("Order")
-    plt.title(algorithm)
+    plt.title(algorithm + str(ordered_request))
 
     # Display the plot
     plt.show()
